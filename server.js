5ede4d5c3eb8b504a7f167f24e9f3e783b6e5405
Evan Limanto <evan@studyform.com>
2017-08-17 21:43:54 -0700

    Add ga, fix paths

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const compression = require('compression');
 const path = require('path');
 
 app.use(compression());
-app.use('/static', express.static(path.join(__dirname, 'public/static')));
+app.use('/', express.static(path.join(__dirname, 'public')));
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/public/index.html'));
+  res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 app.listen(process.env.PORT || 5000);
